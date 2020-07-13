d519e5920abf8ac257b548a2aefe27045a0c03e5
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-07-14 01:25:27 +0400

    fix DBConnect

--- src/main/java/model/DBConnect.java
+++ src/main/java/model/DBConnect.java
@@ -10,42 +10,21 @@ public class DBConnect {
     private static final String USER = "postgres";
     private static final String PASS = "IpMan";
 
-    private static Connection connection = null;
+    private Connection connection = null;
     private Statement statement = null;
 
-    public static Connection connect() throws SQLException{
-        try{
-            Class.forName("org.postgresql.Driver").newInstance();
-        }catch(ClassNotFoundException cnfe){
-            System.err.println("Error: "+cnfe.getMessage());
-        }catch(InstantiationException ie){
-            System.err.println("Error: "+ie.getMessage());
-        }catch(IllegalAccessException iae){
-            System.err.println("Error: "+iae.getMessage());
-        }
-
-        connection = DriverManager.getConnection(DB_URL, USER, PASS);
-        return connection;
-    }
-
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
-        if(connection !=null && !connection.isClosed())
-            return connection;
-        connect();
-        return connection;
-    }
-
-    public Connection openConnection(String DBName, String Password) {
+    public Connection openConnection() {
         try {
             //login DB
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/postgres", DBName, Password);
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
             connection.setAutoCommit(false);
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
+
         return connection;
     }
 
