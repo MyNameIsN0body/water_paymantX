376279ec299d7dcafee0684f4bfb91c629ff870a
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-07-14 02:17:05 +0400

    fix DBConnect

--- src/main/java/model/DBConnect.java
+++ src/main/java/model/DBConnect.java
@@ -6,15 +6,22 @@ import java.sql.Statement;
 
 public class DBConnect {
     //  Database credentials
+    private static final String POSTGRES = "org.postgresql.Driver";
     private static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASS = "IpMan";
+
+    private DBConnect(){}
+
+    public static DBConnect getDBConnect() {
+        return new DBConnect();
+    }
 
     private Connection openConnection() {
         Connection connection = null;
         try {
             //login DB
-            Class.forName("org.postgresql.Driver");
+            Class.forName(POSTGRES);
             connection = DriverManager.getConnection(DB_URL, USER, PASS);
             connection.setAutoCommit(false);
         } catch (Exception e) {
