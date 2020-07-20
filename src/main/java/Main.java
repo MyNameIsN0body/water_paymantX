07d8d47dc8e062bc6eb1bea8b4cceec36de3efa5
MyNameIsN0Body <homer_xxx86@mail.ru>
2020-07-20 20:10:29 +0400

    fix Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import controller.UIFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -5,37 +6,28 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
-    private Stage primaryStage;
-    private AnchorPane rootLayout;
+    //private Stage primaryStage;
+    private ResourceBundle resourceBundle = ResourceBundle.getBundle("uiLocale",
+            Locale.getDefault());
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("График оплаты");
-        this.primaryStage.setResizable(false);
+    public void start(Stage primaryStage) throws Exception {
+//        this.primaryStage = primaryStage;
+//        this.primaryStage.setTitle("График оплаты");
+//        this.primaryStage.setResizable(false);
 
+        UIFactory factory = new UIFactory(resourceBundle, primaryStage);
 
-        // Parent root = FXMLLoader.load(getClass().getResource("voda_01.fxml"));
-        // primaryStage.setTitle("График оплаты");
-        //  primaryStage.setResizable(false);
-        // primaryStage.setScene(new Scene(root));
-
-        launchView();
-    }
-    private void launchView() {
-        try{
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("voda_01.fxml"));
-            rootLayout = (AnchorPane) loader.load();
-
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e){
+        try {
+            factory.initUI();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) {
